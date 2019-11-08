5a32395e5b9bd95b9f5e2e547574430f508fd870
fuhuan1991 <huanfu2019@gmail.com>
2019-11-08 17:24:11 -0500

    add problems

--- test.js
+++ test.js
@@ -3,110 +3,81 @@
  * @return {number}
  */
 var slidingPuzzle = function(board) {
+  const len = board[0].length;
   // initialize memory
-  const visited = new Set();
+  const visited = {};
   // initialize task queue
-  const q = [{arr: board, cnt: 0}];
+  const q = ['0|' + toStr(board)];
   while (q.length > 0) {
-    // if (q.length > 8) return '!!!'
-    const r = searchPossibleMoves(q.shift(), q, visited);
-    console.log(r)
+    // console.log(q)
+    // if (q.length > 8) return
+    const r = searchPossibleMoves(q.shift(), q, visited, len);
     if (r > -1) return r;
   }
   return -1;
 };
 
-const searchPossibleMoves = (o, q, visited) => {
-  const {arr, cnt} = o;
-  const key = arr.toString();
-  // console.log(arr)
+const searchPossibleMoves = (str, q, visited, len) => {
+  const cnt = parseInt(str.split('|')[0], 10);
+  const strBoard = str.split('|')[1];
+
   // if this case has already been analyzed, no need to proceed
-  if (visited.has(key)) {
+  if (visited[strBoard] && visited[strBoard] <= cnt) {
     return;
   } else {
-    visited.add(key);
+    visited[strBoard] = cnt;
   }
 
-  // determine blank 0 square's position
-  // and check if current state is the final state
-  let zeroI, zeroJ;
-  let counter = 1;
+  // check if current state is the target will want
+  // and determine blank 0 square's position
+  let zeroPos = strBoard.indexOf('0');
   let complete = true;
-  for (i = 0; i <= arr.length-1; i++) {
-    for (j = 0; j <= arr[0].length-1; j++) {
-      if (arr[i][j] === 0) {
-        zeroI = i;
-        zeroJ = j;
-      }
-      if (counter === arr.length * arr[0].length) counter = 0;
-      if (arr[i][j] !== counter) complete = false;
-      counter++;
-    }
+  for (let i = 0; i <= strBoard.length-2; i++) {
+    if (strBoard[i] !== i + 1 + '') complete = false;
   }
 
   if (complete) return cnt;
 
+  const newCnt = cnt + 1;
+
   // slide the top block
-  if (zeroI > 0) {
-    const newArr = deepClone(arr);
-    swap(newArr, zeroI, zeroJ, zeroI-1, zeroJ);
-    q.push({
-      arr: newArr,
-      cnt: cnt + 1
-    });
+  if (zeroPos >= len) {
+    const newStr = swap(strBoard, zeroPos, zeroPos - len);
+    q.push(newCnt + '|' + newStr);
   }
   // slide the bottom block
-  if (zeroI < arr.length-1) {
-    const newArr = deepClone(arr);
-    swap(newArr, zeroI, zeroJ, zeroI+1, zeroJ);
-    q.push({
-      arr: newArr,
-      cnt: cnt + 1
-    });
+  if (strBoard.length - 1 - zeroPos >= len) {
+    const newStr = swap(strBoard, zeroPos, zeroPos + len);
+    q.push(newCnt + '|' + newStr);
   }
   // slide the block on the left
-  if (zeroJ > 0) {
-    const newArr = deepClone(arr);
-    swap(newArr, zeroI, zeroJ, zeroI, zeroJ-1);
-    q.push({
-      arr: newArr,
-      cnt: cnt + 1
-    });
+  if (zeroPos%len > 0) {
+    const newStr = swap(strBoard, zeroPos, zeroPos - 1);
+    q.push(newCnt + '|' + newStr);
   }
   // slide the block on the right
-  if (zeroJ < arr[0].length-1) {
-    const newArr = deepClone(arr);
-    swap(newArr, zeroI, zeroJ, zeroI, zeroJ+1);
-    // console.log(111,newArr)
-    q.push({
-      arr: newArr,
-      cnt: cnt + 1
-    });
+  if (zeroPos%len < len-1) {
+    const newStr = swap(strBoard, zeroPos, zeroPos + 1);
+    q.push(newCnt + '|' + newStr);
   }
   return -1;
 }
-/**
- * @param {number[][]} arr
- * @param {number} i1
- * @param {number} j1
- * @param {number} i2
- * @param {number} j2
- * @return {void}
- */
-const swap = (arr, i1, j1, i2, j2) => {
-  const temp = arr[i1][j1];
-  arr[i1][j1] = arr[i2][j2];
-  arr[i2][j2] = temp;
-}
-/**
- * @param {number[][]} arr
- * @return {number[][]}
- */
-const deepClone = (arr) => {
-  const r = [];
-  for (let row of arr) {
-    r.push([...row]);
+
+const toStr = (arr) => {
+  let str = '';
+  for (i = 0; i <= arr.length-1; i++) {
+    for (j = 0; j <= arr[0].length-1; j++) {
+      str = str + arr[i][j];
+    }
   }
-  return r;
+  return str;
 }
-console.log(slidingPuzzle([[1,2,3],[4,5,0]]))
+
+const swap = (str, i, j) => {
+  const arr = str.split('');
+  const temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+  return arr.join('');
+}
+
