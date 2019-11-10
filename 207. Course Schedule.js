ae10a2a318eced9c7e3b5a5709e0f63a342410c7
fuhuan1991 <huanfu2019@gmail.com>
2019-11-10 09:41:34 -0500

    add problems

---
+++ 207. Course Schedule.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function(numCourses, prerequisites) {
+
+    const map = new Array(numCourses).fill(0);
+    for (let i = 0; i <= map.length-1; i++) {
+      map[i] = new Array(numCourses).fill(false);
+    }
+  
+    for (let pair of prerequisites) {
+      map[pair[1]][pair[0]] = true;
+    }
+  
+    // console.log(map)
+    const visited = new Array(numCourses).fill('');
+    for (let i = 0; i <= numCourses-1; i++) {
+      if (detectCircle(i, map, visited)) return false;
+    }
+  
+    return true;
+  };
+  
+  const detectCircle = (i, map, visited) => {
+    // console.log(i, visited)
+    if (visited[i] === 'incomplete') return true; 
+    if (visited[i] === 'completed') return false;
+    visited[i] = 'incomplete';
+  
+    for (let ii = 0; ii <= map[i].length-1; ii++) {
+      // console.log(map[i][ii])
+      if (map[i][ii] && detectCircle(ii, map, visited)) return true;
+    }
+  
+    visited[i] = 'completed';
+    return false;
+  }
+  
+  console.log(canFinish(2, [[1,0],[0,1]]))
--- test.js
+++ test.js
@@ -1,35 +1,41 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function(numCourses, prerequisites) {
 
-var minRemoveToMakeValid = function(s) {
-  let counter = 0;
-  let str = s;
-  for (let i = 0; i <= str.length-1; i++) {
-    if (str[i] === '(') {
-      counter ++;
-    }
-    if (str[i] === ')') {
-      if (counter > 0) {
-        counter--;
-      } else {
-        str = str.slice(0, i) + '#' + str.slice(i+1);
-      }
-    }
+  const map = new Array(numCourses).fill(0);
+  for (let i = 0; i <= map.length-1; i++) {
+    map[i] = new Array(numCourses).fill(false);
   }
-  counter = 0;
-  // console.log(str)
-  for (let i = str.length-1; i >=0; i--) {
-    if (str[i] === '(') {
-      if (counter > 0) {
-        counter--;
-      } else {
-        str = str.slice(0, i) + '#' + str.slice(i+1);
-      }
-    }
-    if (str[i] === ')') {
-      counter++;
-    }
+
+  for (let pair of prerequisites) {
+    map[pair[1]][pair[0]] = true;
   }
-  // console.log(str)
-  return str.replace(/#/g, '');
+
+  // console.log(map)
+  const visited = new Array(numCourses).fill('');
+  for (let i = 0; i <= numCourses-1; i++) {
+    if (detectCircle(i, map, visited)) return false;
+  }
+
+  return true;
 };
 
-// minRemoveToMakeValid('((((sg)fa))')
+const detectCircle = (i, map, visited) => {
+  // console.log(i, visited)
+  if (visited[i] === 'incomplete') return true; 
+  if (visited[i] === 'completed') return false;
+  visited[i] = 'incomplete';
+
+  for (let ii = 0; ii <= map[i].length-1; ii++) {
+    // console.log(map[i][ii])
+    if (map[i][ii] && detectCircle(ii, map, visited)) return true;
+  }
+
+  visited[i] = 'completed';
+  return false;
+}
+
+console.log(canFinish(2, [[1,0],[0,1]]))
