04f6cfb13191886f7141548a2f46e313b00647f1
fuhuan1991 <huanfu2019@gmail.com>
2019-11-11 16:30:11 -0500

    402. Remove K Digits

---
+++ 402. Remove K Digits.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} num
+ * @param {number} k
+ * @return {string}
+ */
+var removeKdigits = function(num, k) {
+    if (k === num.length) return '0';
+    const stack = [];
+    for (let char of num) {
+      while (k > 0 && stack.length > 0 && stack[stack.length-1] > parseInt(char, 10)) {
+        k--;
+        stack.pop();
+      }
+      stack.push(parseInt(char, 10));
+    }
+    while (k > 0) {
+      k--;
+      stack.pop();
+    }
+    
+    while (stack[0] === 0) {
+      stack.shift();
+    }
+    
+    if(stack.length === 0) return '0';
+  
+    return stack.join('');
+  };
+  
+  // console.log(removeKdigits('1234', 2));
--- test.js
+++ test.js
@@ -1,62 +1,30 @@
 /**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number}
+ * @param {string} num
+ * @param {number} k
+ * @return {string}
  */
-var findMedianSortedArrays = function(nums1, nums2) {
-  return search(nums1, nums2);
+var removeKdigits = function(num, k) {
+  if (k === num.length) return '0';
+  const stack = [];
+  for (let char of num) {
+    while (k > 0 && stack.length > 0 && stack[stack.length-1] > parseInt(char, 10)) {
+      k--;
+      stack.pop();
+    }
+    stack.push(parseInt(char, 10));
+  }
+  while (k > 0) {
+    k--;
+    stack.pop();
+  }
+  
+  while (stack[0] === 0) {
+    stack.shift();
+  }
+  
+  if(stack.length === 0) return '0';
+
+  return stack.join('');
 };
 
-const median = (A) => {
-  if (A.length % 2 !== 0) {
-    // odd number
-    return A[(A.length + 1) / 2 - 1];
-  } else {
-    // even number
-    const indexRight = Math.floor(A.length / 2);
-    return (A[indexRight] + A[indexRight - 1])/2;
-  }
-}
-
-const search = (A, B) => {
-  console.log(A, B)
-  if (A.length === 1 || B.length === 1 || A.length === 0 || B.length === 0) {
-    return median(A.concat(B).sort((a,b) => a-b));
-  }
-  if (A.length + B.length <= 4) {
-    const C = A.concat(B).sort((a,b) => a-b);
-    return median(C);
-  }
-  // if (A.length === 2 && B.length === 2) {
-  //   return (Math.max(A[0], B[0]) + Math.min(A[1], B[1]))/2;
-  // }
-  const m1 = median(A);
-  const m2 = median(B);
-
-  if (m1 === m2) return m1;
-  if (m1 > m2) return search(leftHalf(A), rightHalf(B));
-  if (m1 < m2) return search(rightHalf(A), leftHalf(B));
-}
-const leftHalf = (A) => {
-  if (A.length === 1) {
-    return A;
-  } else if (A.length%2 === 0){
-    return A.slice(0, A.length/2);
-  } else {
-    return A.slice(0, (A.length+1)/2);
-  }
-}
-
-const rightHalf = (A) => {
-  if (A.length === 1) {
-    return A;
-  } else if (A.length%2 === 0){
-    return A.slice(A.length/2);
-  } else {
-    return A.slice((A.length-1)/2);
-  }
-}
-
-// console.log(med.ian([1,3,4]) )
-// console.log(rightHalf([1]))
-console.log(findMedianSortedArrays([1,2], [3,4,5,6]))
+// console.log(removeKdigits('1234', 2));
