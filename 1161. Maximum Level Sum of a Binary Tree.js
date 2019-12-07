b34de9abab913c40e177b85b6ae7b24c0f244950
fuhuan1991 <huanfu2019@gmail.com>
2019-12-07 10:11:37 -0500

    1161. Maximum Level Sum of a Binary Tree

---
+++ 1161. Maximum Level Sum of a Binary Tree.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxLevelSum = function(root) {
+    const level = [root];
+    let max = -Infinity;
+    let index = 1;
+    let maxIndex = 1;
+    while (level.length > 0) {
+      const sum = getSumAndUpdateLevel(level);
+      if (sum > max) {
+          max = sum;
+          maxIndex = index
+      }
+      index++;
+    }
+    return maxIndex;
+  };
+  
+  const getSumAndUpdateLevel = (level) => {
+    const len = level.length;
+    let sum = 0;
+    let i = 1;
+    while (i <= len) {
+      const node = level.shift();
+      sum += node.val;
+      if (node.left) level.push(node.left);
+      if (node.right) level.push(node.right);
+      i++;
+    }
+    return sum;
+  }
+  
+  
+  
--- test.js
+++ test.js
@@ -1,20 +1,42 @@
 /**
- * @param {number} num
- * @return {string}
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
  */
-var encode = function(num) {
-  if (num === 0) return '';
-  let start = 1;
-  let len = 1;
-  let end = start + Math.pow(2, len) - 1;
-  while (end < num) {
-    len++;
-    start = end + 1;
-    end = start + Math.pow(2, len) - 1;
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxLevelSum = function(root) {
+  const level = [root];
+  let max = -Infinity;
+  let index = 1;
+  let maxIndex = 1;
+  while (level.length > 0) {
+    const sum = getSumAndUpdateLevel(level);
+    if (sum > max) {
+        max = sum;
+        maxIndex = index
+    }
+    index++;
   }
-  let r = (num - start).toString(2);
-  while (r.length < len) {
-    r = '0' + r;
-  }
-  return r;
+  return maxIndex;
 };
+
+const getSumAndUpdateLevel = (level) => {
+  const len = level.length;
+  let sum = 0;
+  let i = 1;
+  while (i <= len) {
+    const node = level.shift();
+    sum += node.val;
+    if (node.left) level.push(node.left);
+    if (node.right) level.push(node.right);
+    i++;
+  }
+  return sum;
+}
+
+
