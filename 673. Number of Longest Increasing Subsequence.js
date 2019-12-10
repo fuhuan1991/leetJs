c3a0928ba8abcbcd912010a7a717af5f33f17acb
fuhuan1991 <huanfu2019@gmail.com>
2019-12-10 11:37:51 -0500

    673. Number of Longest Increasing Subsequence

---
+++ 673. Number of Longest Increasing Subsequence.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNumberOfLIS = function(nums) {
+    const dp_len = new Array(nums.length).fill(0);
+    const dp_num = new Array(nums.length).fill(0);
+    dp_len[nums.length-1] = 1;
+    dp_num[nums.length-1] = 1;
+    let globalMaxLen = 1;
+    let globalMaxNum = 0;
+  
+    for (let i = nums.length - 2; i >= 0; --i) {
+      const current = nums[i];
+      let maxLen = 0;
+      let counter = 0;
+  
+      for (j = i; j <= nums.length-1; ++j) {
+        if (nums[j] > current) {
+          maxLen = Math.max(maxLen, dp_len[j]);
+        }
+      }
+  
+      dp_len[i] = maxLen + 1;
+      
+      for (j = i; j <= nums.length-1; ++j) {
+        if (dp_len[j] === maxLen && nums[j] > current) {
+          counter += dp_num[j];
+        }
+      }
+  
+      dp_num[i] = counter === 0? 1 : counter;
+  
+      globalMaxLen = Math.max(globalMaxLen, dp_len[i]);
+      console.log(dp_len.toString(), dp_num.toString())
+    }
+  
+    for (let i = 0; i <= nums.length-1; ++i) {
+      if (dp_len[i] === globalMaxLen) {
+        globalMaxNum += dp_num[i];
+      }
+    }
+    return globalMaxNum;
+  };
+  
+  findNumberOfLIS([1,2,4,3,5,4,7,2]);
--- test.js
+++ test.js
@@ -1,24 +1,46 @@
 /**
- * @param {string} s
- * @param {string[]} wordDict
- * @return {boolean}
+ * @param {number[]} nums
+ * @return {number}
  */
-var wordBreak = function(s, wordDict) {
-    const len = s.length;
-    const dp = new Array(len+1).fill(false);
-    dp[0] = true;
-    
-    for (let i = 1; i <= len; i++) {
-        for (let word of wordDict) {
-            const lastIndex = i - word.length;
-            if (lastIndex >= 0 && dp[lastIndex] === true) {
-                const subString = s.slice(lastIndex, i);
-                if (subString === word) {
-                    dp[i] = true;
-                }
-            }
-        }
+var findNumberOfLIS = function(nums) {
+  const dp_len = new Array(nums.length).fill(0);
+  const dp_num = new Array(nums.length).fill(0);
+  dp_len[nums.length-1] = 1;
+  dp_num[nums.length-1] = 1;
+  let globalMaxLen = 1;
+  let globalMaxNum = 0;
+
+  for (let i = nums.length - 2; i >= 0; --i) {
+    const current = nums[i];
+    let maxLen = 0;
+    let counter = 0;
+
+    for (j = i; j <= nums.length-1; ++j) {
+      if (nums[j] > current) {
+        maxLen = Math.max(maxLen, dp_len[j]);
+      }
     }
-// console.log(dp)
-    return dp[len];
+
+    dp_len[i] = maxLen + 1;
+    
+    for (j = i; j <= nums.length-1; ++j) {
+      if (dp_len[j] === maxLen && nums[j] > current) {
+        counter += dp_num[j];
+      }
+    }
+
+    dp_num[i] = counter === 0? 1 : counter;
+
+    globalMaxLen = Math.max(globalMaxLen, dp_len[i]);
+    console.log(dp_len.toString(), dp_num.toString())
+  }
+
+  for (let i = 0; i <= nums.length-1; ++i) {
+    if (dp_len[i] === globalMaxLen) {
+      globalMaxNum += dp_num[i];
+    }
+  }
+  return globalMaxNum;
 };
+
+findNumberOfLIS([1,2,4,3,5,4,7,2]);
