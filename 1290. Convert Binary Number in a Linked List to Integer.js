026821b2df295f5eaf249f35373874eafa6724ff
fuhuan1991 <huanfu2019@gmail.com>
2019-12-19 10:06:39 -0500

    1290. Convert Binary Number in a Linked List to Integer

---
+++ 1290. Convert Binary Number in a Linked List to Integer.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var getDecimalValue = function(head) {
+    const arr = [];
+    let current = head;
+  
+    while(current) {
+      arr.unshift(current.val);
+      current = current.next;
+    }
+  
+    let base = 1;
+    let result = 0;
+  
+    for (let dig of arr) {
+      result = result + dig * base;
+      base = base * 2;
+    }
+  
+    return result;
+  };
--- test.js
+++ test.js
@@ -1,100 +1,30 @@
 /**
- * @param {character[][]} grid
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
  * @return {number}
  */
-var maxKilledEnemies = function(grid) {
-  if (grid.length === 0) return 0;
-  const hei = grid.length;
-  const len = grid[0].length;
-  const dpH = new Array(hei);
-  const dpV = new Array(hei);
+var getDecimalValue = function(head) {
+  const arr = [];
+  let current = head;
 
-  for (let i = 0; i <= hei-1; i++) {
-    dpH[i] = new Array(len).fill(0);
-    dpV[i] = new Array(len).fill(0);
+  while(current) {
+    arr.unshift(current.val);
+    current = current.next;
   }
 
-  for (let i = 0; i <= hei-1; i++) {
-    for (let j = 0; j <= len-1; j++) {
-      if (grid[i][j] === 'W') {
-        dpH[i][j] = 'W';
-      } else {
-        // 0
-        dpH[i][j] = checkLastH(i, j, dpH, grid);
-      }
-    }
+  let base = 1;
+  let result = 0;
+
+  for (let dig of arr) {
+    result = result + dig * base;
+    base = base * 2;
   }
 
-  for (let j = 0; j <= len-1; j++) {
-    for (let i = 0; i <= hei-1; i++) {
-      if (grid[i][j] === 'W') {
-        dpV[i][j] = 'W';
-      } else {
-        // 0
-        dpV[i][j] = checkLastV(i, j, dpV, grid);
-      }
-    }
-  }
-  // console.log(dpV)
-  // console.log(countEnemyRight(1,1,grid));
-  let max = 0;
-  for (let i = 0; i <= hei-1; i++) {
-    for (let j = 0; j <= len-1; j++) {
-      if (grid[i][j] !== 'E' && grid[i][j] !== 'W' && dpH[i][j] + dpV[i][j] > max) {
-        max = dpH[i][j] + dpV[i][j];
-      }
-    }
-  }
-  return max;
+  return result;
 };
-
-const checkLastH = (i, j, dpH, grid) => {
-  let currentJ = j-1;
-  while (true) {
-    if (currentJ < 0 || dpH[i][currentJ] === 'W') {
-      return countEnemyRight(i, j, grid);
-    } else {
-      return dpH[i][currentJ];
-    }
-  }
-}
-
-const checkLastV = (i, j, dpV, grid) => {
-  let currentI = i-1;
-  while (true) {
-    if (currentI < 0 || dpV[currentI][j] === 'W') {
-      return countEnemyDown(i, j, grid);
-    } else {
-      return dpV[currentI][j];
-    }
-  }
-}
-
-const countEnemyRight = (i, j, grid) => {
-  let currentJ = j;
-  let counter = 0;
-  while (true) {
-    if (currentJ > grid[0].length-1 || grid[i][currentJ] === 'W') {
-      return counter;
-    } else {
-      if (grid[i][currentJ] === 'E') counter ++;
-      currentJ++;
-    }
-  }
-}
-
-const countEnemyDown = (i, j, grid) => {
-  let currentI = i;
-  let counter = 0;
-  while (true) {
-    if (currentI > grid.length-1 || grid[currentI][j] === 'W') {
-      return counter;
-    } else {
-      if (grid[currentI][j] === 'E') counter ++;
-      currentI++;
-    }
-  }
-}
-
-
-// maxKilledEnemies([["0","E","0","0"],["E","0","W","E"],["0","E","0","0"]]);
