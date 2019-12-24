51b2ebba2b28b8349de7dae5c5c6d2e0ebafb4e2
fuhuan1991 <huanfu2019@gmail.com>
2019-12-24 18:23:54 -0500

    1105. Filling Bookcase Shelves

--- test.js
+++ test.js
@@ -1,18 +1,29 @@
-var longestCommonSubsequence = function(text1, text2) {
-  let temp = [];
-  let max = 0;
-  for(let i = 0; i <= text1.length; i++) {
-      temp.push(new Array(text2.length + 1).fill(0));
+/**
+ * @param {number[][]} books
+ * @param {number} shelf_width
+ * @return {number}
+ */
+var minHeightShelves = function(books, shelf_width) {
+  const dp = [0, books[0][1]];
+  const N = books.length;
+
+  for (let i = 2; i <= N; i++) {
+    dp[i] = dp[i-1] + books[i-1][1];
+    let widthOfNewLevel = books[i-1][0];
+    let heightOfNewLevel = books[i-1][1];
+
+    for (let j = i-1; j > 0; j--) {
+      
+      if (widthOfNewLevel + books[j-1][0] > shelf_width) break;
+      widthOfNewLevel = widthOfNewLevel + books[j-1][0];
+      heightOfNewLevel = Math.max(heightOfNewLevel, books[j-1][1]);
+      dp[i] = Math.min(dp[i], dp[j-1] + heightOfNewLevel);
+    }
+    console.log(dp)
   }
-  for(let i = 1; i < temp.length; i++) {
-      for(let j = 1; j < temp[0].length; j++) {
-          if(text1[i-1] === text2[j-1]) {
-              temp[i][j] = temp[i-1][j-1] + 1
-          } else {
-              temp[i][j] = Math.max(temp[i-1][j], temp[i][j-1]);
-          }
-          max = Math.max(max, temp[i][j]);
-      }
-  }
-  return max;
+
+  return dp[N];
 };
+
+minHeightShelves([[7,3],[8,7],[2,7],[2,5]],10);
+//,
