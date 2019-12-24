d236392be1a4fa48b647c03fcd7b72a7263361b1
fuhuan1991 <huanfu2019@gmail.com>
2019-12-24 11:41:48 -0500

    1273. Delete Tree Nodes

--- test.js
+++ test.js
@@ -1,34 +1,56 @@
 /**
- * @param {number[][]} grid
+ * @param {number} nodes
+ * @param {number[]} parent
+ * @param {number[]} value
  * @return {number}
  */
-var countCornerRectangles = function(grid) {
-  const height = grid.length;
-  const length = grid[0].length;
-  let result = 0;
+var deleteTreeNodes = function(nodes, parent, value) {
+  const nodeMap = {};
+  const blockedNodes = {};
 
-  const count = new Array(height).fill(0);
-  for (let i = 0; i <= height-1; i++) {
-    count[i] = new Array(length).fill(0);
-  }
-
-  for (let i = 0; i <= height - 1; i++) {
-    const currentRow = grid[i];
-
-    for (let col1 = 0; col1 <= length-2; col1++) {
-      if (currentRow[col1] === 1) {
-        for (let col2 = col1 + 1; col2 <= length-1; col2++) {
-          if (currentRow[col1] === 1 && currentRow[col2] === 1) {
-            result += count[col1][col2];
-            count[col1][col2]++;
-          }
-        }
-      } 
+  for (let i = 0; i <= parent.length - 1; i++) {
+    const p = parent[i];
+    if (nodeMap[p] === undefined) {
+      nodeMap[p] = [i];
+    } else {
+      nodeMap[p].push(i);
     }
   }
+  analyzeNode(nodeMap, 0, value, blockedNodes);
 
+  // run a BFS, count the number of node left.
+  const queue = [0];
+  let result = 0;
+  
+  while (queue.length > 0) {
+    const index = queue.shift();
+    if (blockedNodes[index] === undefined) {
+      result++;
+      if (nodeMap[index] !== undefined) {
+        for (let child of nodeMap[index]) {
+          queue.push(child);
+        }
+      }
+    }
+  }
   return result;
+  // console.log(blockedNodes)
 };
 
+const analyzeNode = (nodeMap, index, value, blockedNodes) => {
+  if (nodeMap[index] === undefined) {
+    // this node has no children
+    if (value[index] === 0) blockedNodes[index] = true;
+    return value[index];
+  } else {
+    let sum = 0;
+    for (let child of nodeMap[index]) {
+      sum += analyzeNode(nodeMap, child, value, blockedNodes);
+    }
+    sum += value[index];
+    if (sum === 0) blockedNodes[index] = true;
+    return sum;
+  }
+}
 
-
+deleteTreeNodes(7, [-1,0,0,1,2,2,2], [1,-2,4,0,-2,-1,-1]);
