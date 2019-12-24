0f20e75993d7f6e39cbf625035f53a6168ecb8a4
fuhuan1991 <huanfu2019@gmail.com>
2019-12-24 16:18:02 -0500

    576. Out of Boundary Paths

--- test.js
+++ test.js
@@ -1,33 +1,81 @@
 /**
- * @param {number[][]} A
+ * @param {number} m
+ * @param {number} n
+ * @param {number} N
+ * @param {number} i
+ * @param {number} j
  * @return {number}
  */
-var minFallingPathSum = function(A) {
-  const N = A.length;
-  const dp = new Array(N).fill(0);
-  for (let i = 0; i <= N-1; i++) {
-    dp[i] = new Array(N).fill(0);
-    if (i === N-1) {
-      dp[i] = A[N-1].slice();
+var findPaths = function(m, n, N, i, j) {
+  if (N === 0) return 0;
+  const dp = new Array(m);
+
+  for (let i = 0; i <= m-1; i++) {
+    dp[i] = new Array(n);
+    for (let j = 0; j <= n-1; j++) {
+      dp[i][j] = new Array(N).fill(-1);
     }
   }
-  console.log(dp);
 
-  for (let row = N-2; row >=0; row--) {
-    for (let col = 0; col <= N-1; col++) {
-      if (col === 0) {
-        dp[row][col] = A[row][col] + Math.min(dp[row+1][col], dp[row+1][col+1]);
-      } else if (col === N-1) {
-        dp[row][col] = A[row][col] + Math.min(dp[row+1][col], dp[row+1][col-1]);
+  return helper(i, j, m, n, N, dp);
+  
+};
+
+const helper = (i, j, m, n, moves, dp) => {
+  const M = 1000000007;
+  let r = 0;
+  if (moves === 1) {
+    if (i === 0) r++;
+    if (i === m - 1) r++;
+    if (j === 0) r++;
+    if (j === n-1) r++;
+    return r;
+  } else {
+    if (i === 0) r++;
+    if (i === m - 1) r++;
+    if (j === 0) r++;
+    if (j === n-1) r++;
+
+    if (i > 0) {
+      if (dp[i - 1][j][moves - 1]  > -1) {
+        r += dp[i - 1][j][moves - 1];
       } else {
-        dp[row][col] = A[row][col] + Math.min(dp[row+1][col], dp[row+1][col+1], dp[row+1][col-1]);
+        const temp = helper(i - 1, j, m, n, moves - 1, dp) % M;
+        r = r + temp;
+        dp[i - 1][j][moves - 1] = temp;
       }
     }
+    
+    if (i < m-1) {
+      if (dp[i + 1][j][moves - 1]  > -1) {
+        r += dp[i + 1][j][moves - 1];
+      } else {
+        const temp = helper(i + 1, j, m, n, moves - 1, dp) % M;
+        r = r + temp;
+        dp[i + 1][j][moves - 1] = temp;
+      }
+    }
+
+    if (j > 0) {
+      if (dp[i][j - 1][moves - 1]  > -1) {
+        r += dp[i][j - 1][moves - 1];
+      } else {
+        const temp = helper(i, j - 1, m, n, moves - 1,dp) % M;
+        r = r + temp;
+        dp[i][j - 1][moves - 1] = temp;
+      }
+    }
+
+    if (j < n - 1) {
+      if (dp[i][j + 1][moves - 1]  > -1) {
+        r += dp[i][j + 1][moves - 1];
+      } else {
+        const temp = helper(i, j + 1, m, n, moves - 1,dp) % M;
+        r = r + temp;
+        dp[i][j + 1][moves - 1] = temp;
+      }
+    }
+
+    return r;
   }
-  console.log(dp);
-  let r = dp[0][0];
-  for (let i = 0; i <= N-1; i++) {
-    r = Math.min(r, dp[0][i]);
-  }
-  return r;
-};
+}
