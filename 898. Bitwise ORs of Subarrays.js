9c2529c5fce75218fc10fe7f0bc25e4a510df558
fuhuan1991 <huanfu2019@gmail.com>
2019-12-28 15:21:46 -0500

    898. Bitwise ORs of Subarrays

---
+++ 898. Bitwise ORs of Subarrays.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} A
+ * @return {number}
+ */
+var subarrayBitwiseORs = function(A) {
+    const N = A.length;
+    const set = new Set();
+    let lastRowSet = new Set();
+    set.add(A[N-1]);
+    lastRowSet.add(A[N-1]);
+    
+    for (let i = N-1; i >= 0; --i) {
+      const newSet = new Set();
+      const I = lastRowSet.values();
+      let o = I.next();
+      newSet.add(A[i]);
+      set.add(A[i]);
+  
+      while (!o.done) {
+        const newValue = o.value | A[i];
+        newSet.add(newValue);
+        set.add(newValue);
+        o = I.next();
+      }
+      lastRowSet = newSet;
+    }
+  
+    return set.size;
+  };
--- test.js
+++ test.js
@@ -1,64 +1,29 @@
 /**
- * @param {number[]} status
- * @param {number[]} candies
- * @param {number[][]} keys
- * @param {number[][]} containedBoxes
- * @param {number[]} initialBoxes
+ * @param {number[]} A
  * @return {number}
  */
-var maxCandies = function(status, candies, keys, containedBoxes, initialBoxes) {
-  let boxes_available = {};
-  const keys_available = {};
-  let final_candies = 0;
+var subarrayBitwiseORs = function(A) {
+  const N = A.length;
+  const set = new Set();
+  let lastRowSet = new Set();
+  set.add(A[N-1]);
+  lastRowSet.add(A[N-1]);
+  
+  for (let i = N-1; i >= 0; --i) {
+    const newSet = new Set();
+    const I = lastRowSet.values();
+    let o = I.next();
+    newSet.add(A[i]);
+    set.add(A[i]);
 
-  for (let index of initialBoxes) {
-    boxes_available[index] = true;
+    while (!o.done) {
+      const newValue = o.value | A[i];
+      newSet.add(newValue);
+      set.add(newValue);
+      o = I.next();
+    }
+    lastRowSet = newSet;
   }
 
-  while (true) {
-    // open boxes with keys and then throw away those keys;
-    for (let index in keys_available) {
-      // const index = keys_available[i];
-      if (boxes_available[index]) {
-        status[index] = 1;
-        delete keys_available[index];
-      }
-    }
-    // find out the boxes that you can open instantly
-    const boxes_can_be_opened = [];
-    for (let index in boxes_available) {
-      // const index = boxes_available[i];
-      if (status[index] === 1) {
-        boxes_can_be_opened.push(index);
-        delete boxes_available[index];
-      }
-    }
-    console.log(boxes_available, keys_available, boxes_can_be_opened)
-
-    if (boxes_can_be_opened.length === 0) break;
-
-    for (let index of boxes_can_be_opened) {
-      // open the box with a specific index
-      // get candies
-      final_candies += candies[index];
-      // update boxes_available
-      for (let i of containedBoxes[index]) {
-        boxes_available[i] = true;
-      }
-      // keys_available
-      for (let i of keys[index]) {
-        keys_available[i] = true;
-      }
-    }
-  }
-
-  return final_candies;
+  return set.size;
 };
-
-
-console.log(maxCandies(
-  [1,0,1,0],
-  [7,5,4,100],
-  [[],[],[1],[]],
-  [[1,2],[3],[],[]],
-  [0]))
