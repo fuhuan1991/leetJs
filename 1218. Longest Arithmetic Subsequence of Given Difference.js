65b09318b9b3b6eed04e31c05dbcbfdd674e59bf
fuhuan1991 <huanfu2019@gmail.com>
2019-12-30 17:01:03 -0500

    1218. Longest Arithmetic Subsequence of Given Difference

---
+++ 1218. Longest Arithmetic Subsequence of Given Difference.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} arr
+ * @param {number} difference
+ * @return {number}
+ */
+var longestSubsequence = function(arr, difference) {
+    const dp = {};
+    let max = 1;
+    
+    for (let integer of arr) {
+      const target = integer - difference;
+      let oldValue = 0;
+      if (dp[target.toString] !== undefined) oldValue = dp[target.toString];
+  
+      dp[integer.toString()] = oldValue + 1;
+      max = Math.max(max, oldValue + 1);
+    }
+  
+    return max;
+  };
--- test.js
+++ test.js
@@ -1,23 +1,20 @@
 /**
- * @param {number[]} nums
+ * @param {number[]} arr
+ * @param {number} difference
  * @return {number}
  */
-var maxSumDivThree = function(nums) {
-  const dp = new Array(3);
-  dp[0] = new Array(nums.length).fill(0);
-  dp[1] = new Array(nums.length).fill(0);
-  dp[2] = new Array(nums.length).fill(0);
+var longestSubsequence = function(arr, difference) {
+  const dp = {};
+  let max = 1;
+  
+  for (let integer of arr) {
+    const target = integer - difference;
+    let oldValue = 0;
+    if (dp[target.toString] !== undefined) oldValue = dp[target.toString];
 
-  for (let pos = 0; pos <= nums.length-1; pos++) {
-    const mod = nums[pos]%3;
-    if (pos === 0) {
-      dp[mod][0] = nums[0];
-    } else {
-      dp[0][pos] = Math.max(dp[(3-mod)%3][pos-1] + nums[pos], dp[0][pos-1]);
-      dp[1][pos] = Math.max(dp[(4-mod)%3][pos-1] + nums[pos], dp[1][pos-1]);
-      dp[2][pos] = Math.max(dp[(5-mod)%3][pos-1] + nums[pos], dp[2][pos-1]);
-    }
+    dp[integer.toString()] = oldValue + 1;
+    max = Math.max(max, oldValue + 1);
   }
 
-  return dp[0][nums.length-1];
+  return max;
 };
