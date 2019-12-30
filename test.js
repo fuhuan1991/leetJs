c29191565397de671d55ddf9c206163580c8facf
fuhuan1991 <huanfu2019@gmail.com>
2019-12-30 15:05:28 -0500

    One Edit Distance

--- test.js
+++ test.js
@@ -1,44 +1,47 @@
 /**
- * @param {string} s1
- * @param {string} s2
- * @return {number}
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
  */
-var minimumDeleteSum = function(s1, s2) {
-  const N = s1.length;
-  const M = s2.length;
-  const dp = new Array(N+1);
-  for (let i = 0; i <= N; i++) {
-    dp[i] = new Array(M+1).fill(0);
-  }
-// console.log(dp);
-  let sum = 0;
-  for (let start = M-1; start >= 0; start --) {
-    dp[N][start] = s2.charCodeAt(start) + sum;
-    sum += s2.charCodeAt(start);
-  }
-  sum = 0;
-  for (let start = N-1; start >= 0; start --) {
-    dp[start][M] = s1.charCodeAt(start) + sum;
-    sum += s1.charCodeAt(start);
-  }
+var isOneEditDistance = function(s, t) {
+  // if (s.length + t.length === 1) {
+  //   return true;
+  // }
+  let i = 0;
+  let j = 0;
+  // let edited = false;
 
-  // console.log(dp);
-  for (let start1 = N-1; start1 >= 0; start1--) {
-    for (let start2 = M-1; start2 >= 0; start2--) {
-      // console.log(start1,start2)
-      if (s1.charCodeAt(start1) === s2.charCodeAt(start2)) {
-        dp[start1][start2] = dp[start1 + 1][start2 + 1];
-      } else {
-        dp[start1][start2] = Math.min(
-          dp[start1][start2+1] + s2.charCodeAt(start2),
-          dp[start1+1][start2] + s1.charCodeAt(start1)
-        );
-        // console.log(dp[start1][start2+1] + s2.charCodeAt(start2), dp[start1+1][start2] + s1.charCodeAt(start1))
-      }
+  while (i <= s.length-1 && j <= t.length-1) {
+    if (s[i] === t[j]) {
+      i++;
+      j++;
+    } else {
+      // edited = true;
+      //delete
+      const r1 = secondChance(s, t, i+1, j);
+      //insert
+      const r2 = secondChance(s, t, i, j+1);
+      //replace
+      const r3 = secondChance(s, t, i+1, j+1);
+      return r1 || r2 || r3;
     }
-  } 
-  console.log(dp);
-  return dp[0][0];
+  }
+  if (Math.abs(s.length-t.length) === 1) return true;
+  return false;
 };
 
-minimumDeleteSum('delete','leet')
+const secondChance = (s, t, i, j) => {
+  while (i <= s.length-1 && j <= t.length-1) {
+    if (s[i] === t[j]) {
+      i++;
+      j++;
+    } else {
+      return false;
+    }
+  }
+  if (s.length - i === t.length - j) {
+    return true;
+  } else {
+    return false;
+  }
+} 
