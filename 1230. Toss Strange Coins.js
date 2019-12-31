4abfa53a85c2fd63d1e736048f052fcb911f9b17
fuhuan1991 <huanfu2019@gmail.com>
2019-12-30 19:38:49 -0500

    Toss Strange Coins

---
+++ 1230. Toss Strange Coins.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} prob
+ * @param {number} target
+ * @return {number}
+ */
+var probabilityOfHeads = function(prob, target) {
+    const N = prob.length;
+    const dp = new Array(N+1);
+    prob.unshift(0);
+  
+    for (let i = 0; i <= N; i++) {
+      dp[i] = new Array(N+1).fill(0);
+    }
+  
+    // console.log(dp)
+    let allTail = 1;
+    let allHead = 1;
+    for (let coins = 1; coins <= N; coins++) {
+      allHead = allHead * prob[coins];
+      allTail = allTail * (1 - prob[coins]);
+      for (let t = 0; t <= Math.min(target, coins); t++) {
+        if (t === 0) {
+          dp[coins][t] = allTail;
+        } else if (coins === t) {
+          dp[coins][t] = allHead;
+        } else {
+          dp[coins][t] = dp[coins-1][t] * (1-prob[coins]) + dp[coins-1][t-1] * prob[coins];
+        }
+      } 
+    }
+    // console.log(dp);
+    return dp[N][target];
+  };
+  
+  // probabilityOfHeads([0.1,0.2,0.3,0.4,0.5],2)
--- test.js
+++ test.js
@@ -1,61 +1,35 @@
 /**
- * @param {number} n
- * @param {number[]} rollMax
+ * @param {number[]} prob
+ * @param {number} target
  * @return {number}
  */
-var dieSimulator = function(n, rollMax) {
-  const mem = new Array(6);
-  for (let i = 0; i <= 5; i++) {
-    mem[i] = new Array(16);
-    for (let j = 0; j <= 15; j++) {
-      mem[i][j] = [];
-    }
-  }
-  return roll(null, 0, n, rollMax, mem);
-  // console.log(mem)
-};
+var probabilityOfHeads = function(prob, target) {
+  const N = prob.length;
+  const dp = new Array(N+1);
+  prob.unshift(0);
 
-const roll = (lastValue, lastLen, restRoll, rollMax, mem) => {
-  const M = Math.pow(10, 9) + 7;
-  if (restRoll <= 0) {
-    return 0;
+  for (let i = 0; i <= N; i++) {
+    dp[i] = new Array(N+1).fill(0);
   }
 
-  if (lastValue && mem[lastValue-1][lastLen][restRoll]) {
-    return mem[lastValue-1][lastLen][restRoll];
-  }
-
-  let r = 0;
-
-  for (let v = 1; v <= 6; v++) {
-    if (v === lastValue && rollMax[lastValue-1] < lastLen + 1) {
-      // console.log(v, lastValue, lastLen)
-      continue;
-    } else {
-      if (v === lastValue) {
-        const partialResult = roll(lastValue, lastLen+1, restRoll-1, rollMax, mem);
-        if (partialResult === 0) {
-          r++;
-        } else {
-          r = r + partialResult;
-        }
+  // console.log(dp)
+  let allTail = 1;
+  let allHead = 1;
+  for (let coins = 1; coins <= N; coins++) {
+    allHead = allHead * prob[coins];
+    allTail = allTail * (1 - prob[coins]);
+    for (let t = 0; t <= Math.min(target, coins); t++) {
+      if (t === 0) {
+        dp[coins][t] = allTail;
+      } else if (coins === t) {
+        dp[coins][t] = allHead;
       } else {
-        const partialResult = roll(v, 1, restRoll-1, rollMax, mem);
-        if (partialResult === 0) {
-          r++;
-        } else {
-          r = r + partialResult;
-        }
+        dp[coins][t] = dp[coins-1][t] * (1-prob[coins]) + dp[coins-1][t-1] * prob[coins];
       }
-    }
+    } 
   }
-
-  if (lastValue) {
-    mem[lastValue-1][lastLen][restRoll] = r%M;
-    // console.log(lastValue, lastLen, restRoll, r)
-  }
-
-  return r%M;
+  // console.log(dp);
+  return dp[N][target];
 };
 
-// console.log(dieSimulator(2, [1,1,2,2,2,3]));
+// probabilityOfHeads([0.1,0.2,0.3,0.4,0.5],2)
