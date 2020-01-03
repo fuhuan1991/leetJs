22ecf3926d54e415d926bc99dcb4554729304a91
fuhuan1991 <huanfu2019@gmail.com>
2020-01-03 11:14:21 -0500

    Count Numbers with Unique Digits

---
+++ 357. Count Numbers with Unique Digits.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countNumbersWithUniqueDigits = function(n) {
+    if (n === 0) return 1;
+    if (n === 1) return 10;
+    let r = 10;
+    for (let i = 2; i <= Math.min(n, 10); i++) {
+      r += helper(i);
+    }
+    return r;
+  };
+  
+  const helper = (k) => {
+    let r = 9;
+    let v = 9;
+    while (k > 1) {
+      r = r * v;
+      v--;
+      k--;
+    } 
+    return r;
+  }
+  
+  // console.log(helper(3))
--- test.js
+++ test.js
@@ -1,33 +1,26 @@
 /**
- * @param {number[]} A
+ * @param {number} n
  * @return {number}
  */
-var lenLongestFibSubseq = function(A) {
-  const N = A.length;
-  const dp = new Array(N);
-  const index = {};
-  let max = 0;
-
-  for (let i = 0; i <= N-1; ++i) {
-    dp[i] = new Array(N).fill(2);
+var countNumbersWithUniqueDigits = function(n) {
+  if (n === 0) return 1;
+  if (n === 1) return 10;
+  let r = 10;
+  for (let i = 2; i <= Math.min(n, 10); i++) {
+    r += helper(i);
   }
-  for (let i = 0; i <= N-1; ++i) {
-    index[A[i]] = i;
-  }
-
-  for (let i = 0; i <= N-1; ++i) {
-    for (let j = i+1; j<= N-1; ++j) {
-      const diff = A[j] - A[i];
-      const x = index[diff];
-      if (x !== undefined && x >= 0 && x <= N-1 && x < i) {
-        dp[i][j] = dp[x][i] + 1;
-        max = Math.max(max, dp[i][j]);
-      }
-
-    }
-  }
-  console.log(dp)
-  return max;
+  return r;
 };
 
-lenLongestFibSubseq([1,2,3,4,5,6,7,8])
+const helper = (k) => {
+  let r = 9;
+  let v = 9;
+  while (k > 1) {
+    r = r * v;
+    v--;
+    k--;
+  } 
+  return r;
+}
+
+// console.log(helper(3))
