9523eeeba5f6ef65ce23196929469174ca6ca4e4
fuhuan1991 <huanfu2019@gmail.com>
2020-01-05 11:06:13 -0500

    Insertion Sort List

---
+++ 147. Insertion Sort List.js
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var insertionSortList = function(head) {
+    if (!head) return null;
+    if (head.next === null) return head;
+  
+    let newNode = head.next;
+    let newHead = head;
+    newHead.next = null;
+  
+    while (newNode) {
+      let temp = newNode.next;
+      newNode.next = null;
+      newHead = insert(newHead, newNode);
+      newNode = temp;
+    }
+  
+    return newHead;
+  };
+  
+  const insert = (head, newNode) => {
+    let current = head;
+    let last = null;
+  
+    while (current && newNode.val > current.val) {
+      last = current;
+      current = current.next;
+    }
+  
+    if (last) {
+      last.next = newNode;
+      newNode.next = current;
+      return head;
+    } else {
+      newNode.next = head;
+      return newNode;
+    }
+  }
--- test.js
+++ test.js
@@ -1,47 +1,47 @@
 /**
- * @param {string} s
- * @param {string} t
- * @return {number}
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
  */
-var numDistinct = function(s, t) {
-  const N = s.length;
-  const M = t.length;
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var insertionSortList = function(head) {
+  if (!head) return null;
+  if (head.next === null) return head;
 
-  if (N === 0 && M > 0) return 0;
-  if (N === 0 && M === 0) return 1;
+  let newNode = head.next;
+  let newHead = head;
+  newHead.next = null;
 
-  const dp = new Array(N);
-  for (let i = 0; i <= N-1; i++) {
-    dp[i] = new Array(M).fill(0);
+  while (newNode) {
+    let temp = newNode.next;
+    newNode.next = null;
+    newHead = insert(newHead, newNode);
+    newNode = temp;
   }
 
-  for (let i = 0; i <= N-1; ++i) {
-    for (let j = 0; j <= M-1; ++j) {
-      if (i < j) {
-        dp[i][j] = 0;
-      } else if (i === j) {
-        if (s.slice(0, i+1) === t.slice(0, j+1)) {
-          dp[i][j] = 1;
-        } else {
-          dp[i][j] = 0;
-        }
-      } else if (j === 0) {
-        if (s.charAt(i) === t.charAt(j)) {
-          dp[i][j] = 1 + dp[i-1][j];
-        } else {
-          dp[i][j] = dp[i-1][j];
-        }
-      } else {
-        if (s.charAt(i) === t.charAt(j)) {
-          dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-        } else {
-          dp[i][j] = dp[i-1][j];
-        }
-      }
-    }
-  }
-console.log(dp)
-  return dp[N-1][M-1];
+  return newHead;
 };
 
-numDistinct("babgbag","bag");
+const insert = (head, newNode) => {
+  let current = head;
+  let last = null;
+
+  while (current && newNode.val > current.val) {
+    last = current;
+    current = current.next;
+  }
+
+  if (last) {
+    last.next = newNode;
+    newNode.next = current;
+    return head;
+  } else {
+    newNode.next = head;
+    return newNode;
+  }
+}
