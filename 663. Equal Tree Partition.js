ecd8fdf39da1e9f0ccaac28bc03d982b0b19595f
fuhuan1991 <huanfu2019@gmail.com>
2020-01-06 11:03:40 -0500

    Equal Tree Partition

---
+++ 663. Equal Tree Partition.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var checkEqualTree = function(root) {
+    if (!root) return false;
+    const sum = getSum(root);
+    const target = sum/2;
+    const result = {value: false};
+    check(root, target, result);
+    return result.value;
+  };
+  
+  const getSum = (node) => {
+    if (!node) return 0;
+    return getSum(node.left) + getSum(node.right) + node.val;
+  }
+  
+  const check = (node, target, result) => {
+    if (!node) return 0;
+    const sum1 = check(node.left, target, result);
+    const sum2 = check(node.right, target, result);
+  
+    if ((sum1 === target && node.left) || (sum2 === target && node.right)) result.value = true;
+  
+    return node.val + sum1 + sum2;
+  }
