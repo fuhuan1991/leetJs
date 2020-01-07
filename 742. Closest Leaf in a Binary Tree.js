119445876bd7e9ea9632b0f2df42cdbbebd50c57
fuhuan1991 <huanfu2019@gmail.com>
2020-01-07 10:05:56 -0500

    Closest Leaf in a Binary Tree

---
+++ 742. Closest Leaf in a Binary Tree.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var findClosestLeaf = function(root, k) {
+    let target = null;
+    connect(root, k, (t) => {
+      target = t;
+    });
+    const Q = [target];
+    const mem = new Set();
+    while(Q.length > 0) {
+      const node = Q.shift();
+      mem.add(node);
+      if (node.left === null && node.right === null) {
+        return node.val;
+      } else {
+        if (node.left && !mem.has(node.left)) Q.push(node.left);
+        if (node.right && !mem.has(node.right)) Q.push(node.right);
+        if (node.parent && !mem.has(node.parent)) Q.push(node.parent);
+      }
+    }
+  };
+  
+  const connect = (node, k, cb) => {
+    if (node.val === k) cb(node);
+    if (node.left) {
+      node.left.parent = node;
+      connect(node.left, k, cb);
+    }
+    if (node.right) {
+      node.right.parent = node;
+      connect(node.right, k, cb);
+    }
+  }
--- test.js
+++ test.js
@@ -7,23 +7,37 @@
  */
 /**
  * @param {TreeNode} root
- * @param {number} val
- * @return {TreeNode}
+ * @param {number} k
+ * @return {number}
  */
-var insertIntoMaxTree = function(root, val) {
-  return insert(root, val);
+var findClosestLeaf = function(root, k) {
+  let target = null;
+  connect(root, k, (t) => {
+    target = t;
+  });
+  const Q = [target];
+  const mem = new Set();
+  while(Q.length > 0) {
+    const node = Q.shift();
+    mem.add(node);
+    if (node.left === null && node.right === null) {
+      return node.val;
+    } else {
+      if (node.left && !mem.has(node.left)) Q.push(node.left);
+      if (node.right && !mem.has(node.right)) Q.push(node.right);
+      if (node.parent && !mem.has(node.parent)) Q.push(node.parent);
+    }
+  }
 };
 
-const insert = (root, val) => {
-  if (!root || val > root.val) {
-    const newNode = {
-      val: val,
-      left: root,
-      right: null,
-    }
-    return newNode;
-  } else {
-    root.right = insert(root.right, val);
-    return root;
+const connect = (node, k, cb) => {
+  if (node.val === k) cb(node);
+  if (node.left) {
+    node.left.parent = node;
+    connect(node.left, k, cb);
+  }
+  if (node.right) {
+    node.right.parent = node;
+    connect(node.right, k, cb);
   }
 }
