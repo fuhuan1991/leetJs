e362c7145c5001724269e7060133571ee217d191
fuhuan1991 <huanfu2019@gmail.com>
2020-01-07 15:39:34 -0500

    Minimum Cost Tree From Leaf Values

---
+++ 1130. Minimum Cost Tree From Leaf Values.js
@@ -0,0 +1,49 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var mctFromLeafValues = function(arr) {
+    const N = arr.length;
+    const maxArr = new Array(N);
+    const dp = new Array(N);
+    for (let i = 0; i <= N-1; ++i) {
+      maxArr[i] = new Array(N).fill(0);
+      dp[i] = new Array(N).fill(0);
+    }
+    for (let i = 0; i <= N-1; ++i) {
+      for (let j = i; j <= N-1; ++j) {
+        maxArr[i][j] = getMax(arr, i, j);
+        if (i + 1 === j) {
+          dp[i][j] = arr[i] * arr[j];
+        }
+      }
+    }
+    // console.log(dp)
+    for (let i = 2; i <= N-1; ++i) {
+      let left = 0;
+      let right = i;
+      while (right <= N-1) {
+        let min  = Infinity;
+        for (let k = left; k < right; ++k) {
+          const v = maxArr[left][k] * maxArr[k+1][right] + dp[left][k] + dp[k+1][right];
+          min = Math.min(min, v);
+        }
+        dp[left][right] = min;
+        left++;
+        right++;
+      }
+    }
+    return dp[0][N-1];
+  };
+  
+  const getMax = (arr, i, j) => {
+    let max = -Infinity;
+    for (let index = i; index <= j; index++) {
+      max = Math.max(max, arr[index]);
+    }
+    return max;
+  }
+  
+  console.log(mctFromLeafValues([6,2,4]));
+  
+  
--- test.js
+++ test.js
@@ -1,52 +1,48 @@
 /**
- * @param {number[][]} matrix
- * @param {number} target
+ * @param {number[]} arr
  * @return {number}
  */
-var numSubmatrixSumTarget = function(matrix, target) {
-  const N = matrix.length;
-  const M = matrix[0].length;
-  const prefixSum = new Array(N);
-  for (let i = 0; i<= N-1; ++i) {
-    prefixSum[i] = new Array(M).fill(0);
+var mctFromLeafValues = function(arr) {
+  const N = arr.length;
+  const maxArr = new Array(N);
+  const dp = new Array(N);
+  for (let i = 0; i <= N-1; ++i) {
+    maxArr[i] = new Array(N).fill(0);
+    dp[i] = new Array(N).fill(0);
   }
-
-  for (let i = 0; i<= N-1; ++i) {
-    for (let j = 0; j <= M-1; ++j) {
-      if (i === 0 && j === 0) {
-        prefixSum[i][j] = matrix[i][j];
-      } else if (i === 0) {
-        prefixSum[i][j] = prefixSum[i][j-1] + matrix[i][j];
-      } else if (j === 0) {
-        prefixSum[i][j] = prefixSum[i-1][j] + matrix[i][j];
-      } else {
-        prefixSum[i][j] = prefixSum[i-1][j] + prefixSum[i][j-1] - prefixSum[i-1][j-1] + matrix[i][j];
+  for (let i = 0; i <= N-1; ++i) {
+    for (let j = i; j <= N-1; ++j) {
+      maxArr[i][j] = getMax(arr, i, j);
+      if (i + 1 === j) {
+        dp[i][j] = arr[i] * arr[j];
       }
     }
   }
-  // console.log(prefixSum);
-  let result = 0;
-  for (let i1 = 0; i1<= N-1; ++i1) {
-    for (let i2 = i1; i2<= N-1; ++i2) {
-      // counter[i] records how many submatrices with sum i has been discovered so far
-      // thoes submatrices has the same height: from i1 to i2
-      const counter = [1];
-      for (let j = 0; j <= M-1; ++j) {
-        const sum = prefixSum[i2][j] - (i1 === 0 ? 0 : prefixSum[i1-1][j]);
-        // console.log('sum ',sum)
-        const diff = sum - target;
-        if (counter[diff] !== undefined) result += counter[diff];
-        if (counter[sum] === undefined) {
-          counter[sum] = 1;
-        } else {
-          counter[sum]++;
-        }
+  // console.log(dp)
+  for (let i = 2; i <= N-1; ++i) {
+    let left = 0;
+    let right = i;
+    while (right <= N-1) {
+      let min  = Infinity;
+      for (let k = left; k < right; ++k) {
+        const v = maxArr[left][k] * maxArr[k+1][right] + dp[left][k] + dp[k+1][right];
+        min = Math.min(min, v);
       }
-      // console.log(i1, i2, result)
+      dp[left][right] = min;
+      left++;
+      right++;
     }
   }
-  // console.log(result)
-  return result;
+  return dp[0][N-1];
 };
 
-// numSubmatrixSumTarget([[0,1,0],[1,1,1],[0,1,0]], 0)
+const getMax = (arr, i, j) => {
+  let max = -Infinity;
+  for (let index = i; index <= j; index++) {
+    max = Math.max(max, arr[index]);
+  }
+  return max;
+}
+
+console.log(mctFromLeafValues([6,2,4]));
+
