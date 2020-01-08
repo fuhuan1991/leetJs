b3490751a98d7b0e768b4c4027f82f4d8151ee36
fuhuan1991 <huanfu2019@gmail.com>
2020-01-08 11:19:41 -0500

    Capacity To Ship Packages Within D Days

---
+++ 1011. Capacity To Ship Packages Within D Days.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} weights
+ * @param {number} D
+ * @return {number}
+ */
+var shipWithinDays = function(weights, D) {
+    let left = 0;
+    let right = sum+1;
+    while (left < right) {
+      const mid = Math.floor((left + right)/2);
+      if (test(weights, preSum, D, mid)) {
+        right = mid;
+      } else {
+        left = mid + 1;
+      }
+    }
+    console.log(left, right)
+    return left;
+  };
+  
+  const test = (weights, D, cap) => {
+    let counter = 1;
+    let sum = 0;
+    for (let i = 0; i <= weights.length-1; ++i) {
+      if (weights[i] > cap) return false;
+      if (sum + weights[i] <= cap) {
+        // use former ship
+        sum += weights[i];
+      } else {
+        // use a new ship
+        counter++;
+        sum = weights[i];
+      }
+    }
+    return counter <= D;
+  }
+  shipWithinDays([1,2,3,1,1], 4);
--- test.js
+++ test.js
@@ -1,54 +1,37 @@
 /**
- * @param {number[]} nums
- * @return {boolean}
+ * @param {number[]} weights
+ * @param {number} D
+ * @return {number}
  */
-var circularArrayLoop = function(nums) {
-  const visited = new Array(nums.length).fill(0);
-  const N = nums.length;
-
-  for (let i = 0; i <= N-1; ++i) {
-    console.log(visited)
-    if (visited[i] === 0) {
-      let path = {};
-      let current = i;
-      let length2Cycle = true;
-
-      while (!path[current]) {
-        
-        path[current] = true;
-        console.log(path, length2Cycle)
-        visited[current] = 1;
-
-        let step = nums[current];
-        while (step < 0) {
-          step += N;
-        }
-        const next = (step + current)%N;
-
-        if (nums[current]%N === 0) length2Cycle = false;
-
-        current = next; 
-      }
-      path = {};
-      let sameDirection = true;
-      if (length2Cycle) {
-        while (!path[current]) {
-          
-          let step = nums[current];
-          while (step < 0) {
-            step += N;
-          }
-          const next = (step + current)%N;
-          // console.log(nums[current], '->', nums[next])
-          if (nums[current] * nums[next] < 0) sameDirection = false;
-          path[current] = true;
-          current = next;
-        }
-      }
-      if (sameDirection && length2Cycle) return true;
+var shipWithinDays = function(weights, D) {
+  let left = 0;
+  let right = sum+1;
+  while (left < right) {
+    const mid = Math.floor((left + right)/2);
+    if (test(weights, preSum, D, mid)) {
+      right = mid;
+    } else {
+      left = mid + 1;
     }
   }
-
-  return false;
+  console.log(left, right)
+  return left;
 };
-console.log(circularArrayLoop([-8,-1,1,7,2]))
+
+const test = (weights, D, cap) => {
+  let counter = 1;
+  let sum = 0;
+  for (let i = 0; i <= weights.length-1; ++i) {
+    if (weights[i] > cap) return false;
+    if (sum + weights[i] <= cap) {
+      // use former ship
+      sum += weights[i];
+    } else {
+      // use a new ship
+      counter++;
+      sum = weights[i];
+    }
+  }
+  return counter <= D;
+}
+shipWithinDays([1,2,3,1,1], 4);
