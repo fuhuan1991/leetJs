d262cbf940c7458a6027c6506c79291998ed7adc
fuhuan1991 <huanfu2019@gmail.com>
2020-01-09 12:29:07 -0500

    Reverse Substrings Between Each Pair of Parentheses

---
+++ 1190. Reverse Substrings Between Each Pair of Parentheses.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseParentheses = function(s) {
+    const stack = [""];
+  
+    for (let i = 0; i <= s.length-1; ++i) {
+      const current = s.charAt(i);
+  
+      if (current === '(') {
+        stack.push('');
+      } else if (97 <= s.charCodeAt(i) && s.charCodeAt(i) <= 122) {
+        stack[stack.length-1] += s.charAt(i);
+      } else if (current === ')') {
+        const str = stack.pop();
+        stack[stack.length-1] += reverseString(str);
+      }
+    }
+    return stack[stack.length-1];
+  
+  };
+  
+  function reverseString(str) {
+    // Step 1. Use the split() method to return a new array
+    var splitString = str.split(""); // var splitString = "hello".split("");
+    // ["h", "e", "l", "l", "o"]
+  
+    // Step 2. Use the reverse() method to reverse the new created array
+    var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+    var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+    return joinArray; // "olleh"
+  }
+  
+  console.log(reverseParentheses("(abcd)"))
+  // console.log(reverseString('abc'))
--- test.js
+++ test.js
@@ -1,22 +1,36 @@
 /**
- * @param {string} S
- * @return {number}
+ * @param {string} s
+ * @return {string}
  */
-var scoreOfParentheses = function(S) {
-  const stack = [0];
-  for (let i = 0; i <= S.length-1; ++i) {
-    if (S[i] === '(') {
-      stack.push(0);
-    } else {
-      const sum = stack[stack.length-1];
-      if (sum === 0) {
-        stack.pop();
-        stack[stack.length-1]++;
-      } else {
-        stack.pop();
-        stack[stack.length-1] += 2 * sum;
-      }
+var reverseParentheses = function(s) {
+  const stack = [""];
+
+  for (let i = 0; i <= s.length-1; ++i) {
+    const current = s.charAt(i);
+
+    if (current === '(') {
+      stack.push('');
+    } else if (97 <= s.charCodeAt(i) && s.charCodeAt(i) <= 122) {
+      stack[stack.length-1] += s.charAt(i);
+    } else if (current === ')') {
+      const str = stack.pop();
+      stack[stack.length-1] += reverseString(str);
     }
   }
-  return stack[0];
+  return stack[stack.length-1];
+
 };
+
+function reverseString(str) {
+  // Step 1. Use the split() method to return a new array
+  var splitString = str.split(""); // var splitString = "hello".split("");
+  // ["h", "e", "l", "l", "o"]
+
+  // Step 2. Use the reverse() method to reverse the new created array
+  var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+  var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+  return joinArray; // "olleh"
+}
+
+console.log(reverseParentheses("(abcd)"))
+// console.log(reverseString('abc'))
