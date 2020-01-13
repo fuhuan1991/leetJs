7b9bc9516425e7e5c4efbbd34dca3626c742c411
fuhuan1991 <huanfu2019@gmail.com>
2020-01-13 10:42:39 -0500

    Minimum Number of Refueling Stops

---
+++ 871. Minimum Number of Refueling Stops.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} target
+ * @param {number} startFuel
+ * @param {number[][]} stations
+ * @return {number}
+ */
+var minRefuelStops = function(target, startFuel, stations) {
+    const N = stations.length;
+    const dp = new Array(N+1).fill(0);
+    // const st = [[0,0], ...stations];
+    dp[0] = startFuel;
+  
+    for (let i = 0; i <= N-1; ++i) {
+      for (let t = i+1; t >= 1; t--) {
+        if (dp[t-1] >= stations[i][0]) {
+          dp[t] = Math.max(dp[t], dp[t-1] + stations[i][1]);
+        }
+      }
+    }
+  
+    for (t = 0; t <= N; t++) {
+      if (dp[t] >= target) return t;
+    }
+  
+    return -1;
+  };
--- test.js
+++ test.js
@@ -1,38 +1,26 @@
 /**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
+ * @param {number} target
+ * @param {number} startFuel
+ * @param {number[][]} stations
+ * @return {number}
  */
-/**
- * @param {ListNode} head
- * @return {number[]}
- */
-var nextLargerNodes = function(head) {
-  const stack = [];
-  const r = [];
-  rec(head, stack, r);
+var minRefuelStops = function(target, startFuel, stations) {
+  const N = stations.length;
+  const dp = new Array(N+1).fill(0);
+  // const st = [[0,0], ...stations];
+  dp[0] = startFuel;
 
-  
-  return r;
+  for (let i = 0; i <= N-1; ++i) {
+    for (let t = i+1; t >= 1; t--) {
+      if (dp[t-1] >= stations[i][0]) {
+        dp[t] = Math.max(dp[t], dp[t-1] + stations[i][1]);
+      }
+    }
+  }
+
+  for (t = 0; t <= N; t++) {
+    if (dp[t] >= target) return t;
+  }
+
+  return -1;
 };
-
-
-const rec = (node, stack, r) => {
-  if (!node) return;
-  rec(node.next, stack, r);
-
-  while (stack.length > 0 && node.val >= stack[0]) {
-    stack.shift();
-  }
-
-  if (stack.length === 0) {
-    r.unshift(0);
-  } else {
-    r.unshift(stack[0])
-  }
-
-  stack.unshift(node.val);
-  return;
-}
