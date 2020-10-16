da27bda8c9fbd358b28d73847b733afb9e66d718
fuhuan1991 <huanfu2019@gmail.com>
2020-10-16 11:28:53 -0400

    binaryInsert_II

--- binaryInsert.js
+++ binaryInsert.js
@@ -19,4 +19,26 @@ function binaryInsert (arr) {
   return r;
 }
 
-binaryInsert([1,2,3])
+function binaryInsert_II (arr) {
+  let r = [];
+  for (let i = 0; i < arr.length; i++) {
+
+    let left = 0;
+    let right = r.length-1;
+    const current = arr[i];
+
+    while (left < right) {
+      const mid = Math.floor((left + right)/2);
+      if (current < r[mid]) {
+        right = mid;
+      } else {
+        left = mid + 1;
+      }
+    }
+    const index = current > r[left]? left + 1 : left
+    r.splice(index, 0, current);
+  }
+  return r;
+}
+
+binaryInsert_II([1,2,3])
